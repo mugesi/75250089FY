be231be66109780ac32a1909c40781b9a7fb0dfc
mugesi <144921064+mugesi@users.noreply.github.com>
2025-03-04 13:10:18 +0100

    Add files via upload

---
+++ Exercises/InClass_function.R
@@ -0,0 +1,44 @@
+
+my_square_function <- function(x) {
+  x^2  
+}
+
+my_square_function <- function(x) {
+  return(x^2)
+}
+
+my_square_function <- function(x) {
+  a <- x^2  
+  a
+}
+
+my_first_function <- function(x) {
+  a <- x^2  
+  if (a < 10) {
+    return(paste("too low:", a))
+  } else if (a >= 10 && a <= 50) {
+    return("acceptable")
+  } else {
+    return("desired")
+  }
+}
+
+
+my_first_function(10)
+
+
+my_sum_function <- function (x, y) {
+  a <- x + y + 5
+  return(a)
+}
+
+my_sum_function(2)
+
+
+
+
+
+
+
+
+
